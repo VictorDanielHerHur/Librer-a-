83b72a32766908aadae596973d3c740bbe27c07a
VictorDanielHerHur <victorherhur014@gmail.com>
2026-08-20 21:09:28 -0600

    Remove ITM functions from inicio.h
    Removed ITM_SendChar and _write functions from inicio.h

--- inicio.h
+++ inicio.h
@@ -16,24 +16,7 @@ uint8_t estado_elevador = 0; // 0=Detenido, 1=Subiendo, 2=Bajando
 uint8_t estado_elevador = 0; // 0=Detenido, 1=Subiendo, 2=Bajando
 int piso_actual = 1;
 
-// ====================== FUNCIONES ITM =========================
-// ====================== FUNCIONES ITM =========================
-void ITM_SendChar(uint8_t character)
-{
-    ITM_TER |= 1U;
-    ITM_TCR |= 1U;
-    DEMCR |= (1U << 24);
-    while((ITM_STIMULUS_PORT0 & 1U) == 0U);
-    ITM_STIMULUS_PORT0 = character;
-}
 
-int _write(int file, char *ptr, int len)
-{
-    int i;
-    (void)file;
-    for(i = 0; i < len; i++) ITM_SendChar((uint8_t)*ptr++);
-    return len;
-}
 
 
 
